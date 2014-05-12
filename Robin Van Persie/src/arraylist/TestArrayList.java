269b80aea5acb25536ac9ff10b76ee4ddc9d3547
tong_za49@hotmail.com <tongza49>
2014-05-12 18:47:48 +0700

    LAB

--- Robin Van Persie/src/arraylist/TestArrayList.java
+++ Robin Van Persie/src/arraylist/TestArrayList.java
@@ -4,34 +4,36 @@ public class TestArrayList {
 
 	public static void main(String[] args) {
 		
-		System.out.println("..........ArrayList..........");
-		ArrayList array = new ArrayList();
-		array.add("1");
-		System.out.println(array);
-		array.add("2");
-		System.out.println(array);
-		array.add("3");
-		System.out.println(array);
+		System.out.println("Test ArrayList");
+		ArrayList list = new ArrayList();
+		list.add("Number 1");
+		list.add("Number 2");
+		list.add("Number 3");
+		list.add("Number 4");
+		list.add("Number 5");
+		System.out.println(list);
 		
-		System.out.println("..........First..........");
+
 		
-		array.addFirst(0);
-		System.out.println("Add.First = "+array);
+		System.out.println("Test ArrayListQueue");
+		ArrayListQueue queue = new ArrayListQueue();
+		queue.enQueue("Number 1");
+		queue.enQueue("Number 2");
+		queue.enQueue("Number 3");
+		queue.enQueue("Number 4");
+		queue.enQueue("Number 5");
+		System.out.println(queue);
 		
-		System.out.println("..........Last..........");
 		
-		array.addLast(4);
-		System.out.println("Add.Last = "+array);
 		
-		System.out.println("..........Size..........");
-		
-		array.size();
-		System.out.println("Size = "+array.size());
-		
-		System.out.println("..........Remove Array 4..........");
-		
-		array.remove(4);
-		System.out.println("Remove = "+array);
+		System.out.println("Test ArrayListStack");
+		ArrayListStack stack = new ArrayListStack();
+		stack.push("Number 1");
+		stack.push("Number 2");
+		stack.push("Number 3");
+		stack.push("Number 4");
+		stack.push("Number 5");
+		System.out.println(stack);
 	}
 
 }
